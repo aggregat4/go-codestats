cad106e84f06ab014a505886b6ed9c46d24df467
terzic <boris.terzic@disy.net>
2023-09-06 11:11:07 +0200

    Adding scc dependencies

--- go.mod
+++ go.mod
@@ -2,3 +2,15 @@ module aggregat4/gocodestats
 
 go 1.20
 
+require github.com/boyter/scc/v3 v3.1.0
+
+require (
+	github.com/dbaggerman/cuba v0.3.2 // indirect
+	github.com/json-iterator/go v1.1.9 // indirect
+	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/minio/blake2b-simd v0.0.0-20160723061019-3f5f724cb5b1 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
+	golang.org/x/text v0.3.0 // indirect
+	gopkg.in/yaml.v2 v2.2.8 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,64 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/boyter/scc/v3/processor"
+)
+
+type statsProcessor struct{}
+
+func (p *statsProcessor) ProcessLine(job *processor.FileJob, currentLine int64, lineType processor.LineType) bool {
+	switch lineType {
+	case processor.LINE_BLANK:
+		fmt.Println(currentLine, "lineType", "BLANK")
+	case processor.LINE_CODE:
+		fmt.Println(currentLine, "lineType", "CODE")
+	case processor.LINE_COMMENT:
+		fmt.Println(currentLine, "lineType", "COMMENT")
+	}
+	return true
+}
+
+// func main() {
+// 	bts, _ := os.ReadFile("main.go")
+
+// 	t := &statsProcessor{}
+// 	filejob := &processor.FileJob{
+// 		Filename: "test.go",
+// 		Language: "Go",
+// 		Content:  bts,
+// 		Callback: t,
+// 		Bytes:    int64(len(bts)),
+// 	}
+
+// 	processor.ProcessConstants() // Required to load the language information and need only be done once
+// 	processor.CountStats(filejob)
+// }
+
 func main() {
-	panic("Hello World")
+	root := "/home/terzic/dev/cad/master/cadenza/"
+	stack := []string{root}
+	err := filepath.WalkDir(root, func(path string, d os.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if d.IsDir() {
+			stack = append(stack, path)
+		}
+		parentPath := ""
+		if len(stack) > 1 {
+			parentPath = stack[len(stack)-2]
+		}
+		fmt.Println(path, parentPath)
+		if d.IsDir() {
+			defer func() { stack = stack[:len(stack)-1] }()
+		}
+		return nil
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
 }
